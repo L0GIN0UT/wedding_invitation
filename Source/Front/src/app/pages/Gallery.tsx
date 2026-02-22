c61e49750f83ad28cfda5c0f0dfe6671391c4075
L0GIN0UT <ivan.login.02@mail.ru>
2026-02-22 18:22:00 +0400

    fix: optimize pictures load

--- Source/Front/src/app/pages/Gallery.tsx
+++ Source/Front/src/app/pages/Gallery.tsx
@@ -289,6 +289,7 @@ export const Gallery: React.FC = () => {
                         src={photo}
                         alt={`Фото ${index + 1}`}
                         className="w-full h-auto transition-transform duration-300 group-hover:scale-110"
+                        loading="lazy"
                       />
                       <div className="absolute inset-0 bg-gradient-to-t from-black/60 via-transparent to-transparent opacity-0 group-hover:opacity-100 transition-opacity duration-300 flex items-end p-4">
                         <button
